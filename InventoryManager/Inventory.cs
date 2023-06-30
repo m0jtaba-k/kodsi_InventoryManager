32f7e7d04a50232519dfd7624cae9806fa13ee06
j.kodsi <j.kodsi@smartsoft-me.com>
2023-06-30 14:41:38 +0300

    moreMethods

--- InventoryManager/Inventory.cs
+++ InventoryManager/Inventory.cs
@@ -5,9 +5,19 @@
         private int currentSize;
         List<Item> items = new List<Item>();
 
-        public void addItem(Item)
+        public void addItem(Item item)
         {
-             
+             items.Add(item);
+        }
+
+        public void deleteItem(Item item)
+        {
+            items.Remove(item);
+        }
+
+        public void updateItem(Item item)
+        {
+
         }
     }
 }
